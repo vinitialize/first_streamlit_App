a37baee2ca5f0f3b09953c12ccd6f4fa7688934e
vinitb <44916315+vinitialize@users.noreply.github.com>
2023-05-17 18:14:15 +0530

    Update streamlit_app.py
    Update position of list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 #Created the main python file
 import streamlit
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 streamlit.title('This is my first streamlit app')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -10,3 +8,5 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
