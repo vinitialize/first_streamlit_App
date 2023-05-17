a07ba518deb21d9d75d36a281b213f8d1f879865
vinitb <44916315+vinitialize@users.noreply.github.com>
2023-05-17 18:07:46 +0530

    Update streamlit_app.py
    added few lines of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 #Created the main python file
 import streamlit
 streamlit.title('This is my first streamlit app')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
