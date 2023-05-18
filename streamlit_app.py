d657a00414cedb79912a3019b29361dda1700bba
vinitb <44916315+vinitialize@users.noreply.github.com>
2023-05-18 18:22:25 +0530

    Update streamlit_app.py
    typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,13 +25,11 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
-    else:
-      streamlit.write('The user entered ', fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-      # Normalizing the JSON using Pandas
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      # Inserting normalized JSON into Dataframe
-      streamlit.dataframe(fruityvice_normalized)
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
   
