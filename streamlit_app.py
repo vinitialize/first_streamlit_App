40b8bde47c0d1acc2a9a50508618eac527bead8c
vinitb <44916315+vinitialize@users.noreply.github.com>
2023-05-18 17:58:44 +0530

    Update streamlit_app.py
    Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,4 @@ streamlit.header("FRUIT LOAD LIST CONTAINS:")
 streamlit.dataframe(my_data_rows)
 #Allow end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like add?')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write('The user entered ', add_my_fruit)
